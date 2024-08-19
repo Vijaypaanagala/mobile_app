56dd2ac20f9a4fe92ca660c72129f766afef6fdd
Prasanthd6 <22pa1a05d6@vishnu.edu.in>
2024-08-19 12:08:53 +0530

    rr

---
+++ lib/pages/animefb.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Anime extends StatelessWidget {
+  const Anime({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/pages/main.dart
+++ lib/pages/main.dart
@@ -1,12 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import './pages/home.dart';
+import 'home.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   const MyApp({super.key});
 
   @override
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowMaterialGrid: false,
       debugShowCheckedModeBanner: false,
-      home: Home(), 
+      home: Home(),
     );
   }
 }
